c1aeda7753b3629e7f2cedfe746a63b5ddc0bf40
jsbean <jbean46@gmail.com>
2016-07-02 20:12:41 +0200

    Add doc comments for Reader

--- SwiftParser/Reader.swift
+++ SwiftParser/Reader.swift
@@ -8,12 +8,36 @@
 
 import Foundation
 
+/**
+ Protocol defining interface for string scanning objects.
+ */
 public protocol Reader {
+    
+    /// Current position of `Reader`.
     var position: Int { get }
     
+    /**
+     Move to given `position`.
+     */
     func seek(position: Int)
+    
+    /**
+     Read the next character.
+     */
     func read() -> Character
+    
+    /**
+     - returns: Substring within the given interval.
+     */
     func substring(startingAt startIndex: Int, endingAt endIndex: Int) -> String
+    
+    /**
+     - returns: `true` if `Reader` has reached the end of the file. Otherwise, `false`.
+     */
     func eof() -> Bool
+    
+    /**
+     - returns: Remaining, unconsumed component of string.
+     */
     func remainder() -> String
 }
